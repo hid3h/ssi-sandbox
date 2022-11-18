63f54d6b48e0efa918b63473c67298e3703d64ce
hid3h <razyetts@hotmail.co.jp>
2022-11-18 18:49:42 +0900

    VCを検証してみたいけどよくわからず

--- verifiable_credentilas/index.js
+++ verifiable_credentilas/index.js
@@ -14,3 +14,26 @@ console.log("did", did)
 
 const verificationMethod = DIDKit.keyToVerificationMethod('key', key);
 console.log("verificationMethod", verificationMethod)
+
+const vc = `{
+  "@context": [
+    "https://www.w3.org/2018/credentials/v1"
+  ],
+  "type": "VerifiableCredential",
+  "credentialSubject": {
+    "id": "urn:uuid:bd198dcf-1034-4a11-9e5b-571afb1225a1"
+  },
+  "issuer": "did:web:demo.spruceid.com",
+  "issuanceDate": "2022-11-16T01:46:04Z",
+  "proof": {
+    "type": "Ed25519Signature2018",
+    "proofPurpose": "assertionMethod",
+    "verificationMethod": "did:web:demo.spruceid.com#_t-v-Ep7AtkELhhvAzCCDzy1O5Bn_z1CVFv9yiRXdHY",
+    "created": "2022-11-16T01:46:04.550Z",
+    "jws": "eyJhbGciOiJFZERTQSIsImNyaXQiOlsiYjY0Il0sImI2NCI6ZmFsc2V9..W5S3KEhAWDOExWIeZ9LZrmcrEABO5AmmczYZMvgZWAQjL4u_8tPnmNDPsQ-Hw82lfcRviezSUc18tlPM77GpCQ"
+  },
+  "expirationDate": "2022-12-16T01:46:04Z"
+}`
+DIDKit.verifyCredential(vc, "{}").then((value) => {
+  console.log("value", value)
+})

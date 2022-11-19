0e828d93f7d2aab0398d9c228503977b78a67dcf
hid3h <razyetts@hotmail.co.jp>
2022-11-19 22:08:27 +0900

    署名して検証はできたけど肝心なのが検証できない

--- verifiable_credentilas/index.ts
+++ verifiable_credentilas/index.ts
@@ -1,8 +1,11 @@
-import { compactVerify } from "jose"
-import { createPublicKey } from "crypto"
+import { compactVerify, CompactSign } from "jose"
+import { createPrivateKey, createPublicKey } from "crypto"
+import bs58 from "bs58"
 
 // 以下のjwsの検証をしたい
-
+// なぜか検証できない
+async function main() {
+// jwsの元ネタ
 // https://demo.spruceid.com/get-example-vc から取得
 // const vc = {
 //   "@context": [
@@ -23,31 +26,71 @@ import { createPublicKey } from "crypto"
 //   },
 //   "expirationDate": "2022-12-16T01:46:04Z"
 // }
-// const proofOption = {
-//   "type": "Ed25519Signature2018",
-//   "proofPurpose": "assertionMethod",
-//   "verificationMethod": "did:web:demo.spruceid.com#_t-v-Ep7AtkELhhvAzCCDzy1O5Bn_z1CVFv9yiRXdHY",
-//   "created": "2022-11-16T01:46:04.550Z",
-// }
 
+  // 参考: https://stackoverflow.com/questions/68612396/sign-and-verify-jws-json-web-signature-with-ed25519-keypair
+  // publickKeyはここから取得: https://demo.spruceid.com/.well-known/did.json
+  const publicKey = createPublicKey({
+    key: {
+      kty: "OKP",
+      crv: "Ed25519",
+      x: "2yv3J-Sf263OmwDLS9uFPTRD0PzbvfBGKLiSnPHtXIU"
+    },
+    format: "jwk"
+  })
+  console.log("publicKey", publicKey)
 
-// 参考: https://stackoverflow.com/questions/68612396/sign-and-verify-jws-json-web-signature-with-ed25519-keypair
-const key = createPublicKey({
-  key: {
-    "kty": "OKP",
-    "crv": "Ed25519",
-    "x": "2yv3J-Sf263OmwDLS9uFPTRD0PzbvfBGKLiSnPHtXIU"
-  },
-  format: "jwk"
-})
-
-const jws = "eyJhbGciOiJFZERTQSIsImNyaXQiOlsiYjY0Il0sImI2NCI6ZmFsc2V9..W5S3KEhAWDOExWIeZ9LZrmcrEABO5AmmczYZMvgZWAQjL4u_8tPnmNDPsQ-Hw82lfcRviezSUc18tlPM77GpCQ"
-const option = {
-  algorithms: ["EdDSA"]
+  const jws = "eyJhbGciOiJFZERTQSIsImNyaXQiOlsiYjY0Il0sImI2NCI6ZmFsc2V9..W5S3KEhAWDOExWIeZ9LZrmcrEABO5AmmczYZMvgZWAQjL4u_8tPnmNDPsQ-Hw82lfcRviezSUc18tlPM77GpCQ"
+  const option = {
+    algorithms: ["EdDSA"]
+  }
+  // compactVerify: https://github.com/panva/jose/blob/main/docs/functions/jws_compact_verify.compactVerify.md#readme
+  const result = await compactVerify(jws, publicKey, option)
+  console.log("result", result)
 }
-// compactVerify: https://github.com/panva/jose/blob/main/docs/functions/jws_compact_verify.compactVerify.md#readme
-compactVerify(jws, key, option).then((res) => {
-  console.log("res", res)
-}).catch((err) => {
-  console.log("err", err)
-})
+
+// 試しに署名して検証してみたもの。
+// これは正常に動作する。
+// 参考: https://stackoverflow.com/questions/68612396/sign-and-verify-jws-json-web-signature-with-ed25519-keypair
+async function signAndVerifyExample() {
+  const publicKeyUint = bs58.decode("A77GCUCZ7FAuXVMKtwwXyFhMa158XsaoGKHYNnJ1q3pv");
+  const privateKeyUint = bs58.decode("BE1VM7rTRJReLsTLLG4JMNX5ozcp7qpmMuRht9zB1UjU");
+
+  const publicKey = createPublicKey({
+    key: {
+      kty: "OKP",
+      crv: "Ed25519",
+      x: btoa(String.fromCharCode(...publicKeyUint))
+    },
+    format: "jwk"
+  });
+  console.log("publicKey", publicKey, btoa(String.fromCharCode(...publicKeyUint)))
+
+  const privateKey = createPrivateKey({
+    key: {
+      kty: "OKP",
+      crv: "Ed25519",
+      x: btoa(String.fromCharCode(...publicKeyUint)),
+      d: btoa(String.fromCharCode(...privateKeyUint)),
+    },
+    format: "jwk"
+  })
+  console.log("privateKey", privateKey)
+
+  const jws = await new CompactSign(
+    new TextEncoder().encode(""),
+  )
+  .setProtectedHeader({ alg: "EdDSA" })
+  .sign(privateKey);
+
+  console.log("jws", jws)
+
+  const option = {
+    algorithms: ["EdDSA"]
+  }
+  // compactVerify: https://github.com/panva/jose/blob/main/docs/functions/jws_compact_verify.compactVerify.md#readme
+  const result = await compactVerify(jws, publicKey, option)
+  console.log("result", result)
+}
+
+// signAndVerifyExample();
+main();

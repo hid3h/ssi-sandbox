d71038c1fc18bc1dda36ec2408eeac01f8154a5a
hid3h <razyetts@hotmail.co.jp>
2022-11-19 16:47:25 +0900

    Unable to dereference DID URL

--- verifiable_credentilas/index.ts
+++ verifiable_credentilas/index.ts
@@ -1,21 +1,6 @@
-const DIDKit = require('didkit-wasm-node');
+import { verifyCredential } from "didkit-wasm-node"
 
-console.log(DIDKit.getVersion());
-
-// To issue credentials and presentations, you need a key.
-// The library provides a function to generate one.
-const key = DIDKit.generateEd25519Key();
-console.log("key", key)
-
-// There are two helpful functions to obtain a DID and the `did:key`
-// `verificationMethod` from the key.
-const did = DIDKit.keyToDID('key', key);
-console.log("did", did)
-
-const verificationMethod = DIDKit.keyToVerificationMethod('key', key);
-console.log("verificationMethod", verificationMethod)
-
-const vc = `{
+const vc = {
   "@context": [
     "https://www.w3.org/2018/credentials/v1"
   ],
@@ -33,7 +18,15 @@ const vc = `{
     "jws": "eyJhbGciOiJFZERTQSIsImNyaXQiOlsiYjY0Il0sImI2NCI6ZmFsc2V9..W5S3KEhAWDOExWIeZ9LZrmcrEABO5AmmczYZMvgZWAQjL4u_8tPnmNDPsQ-Hw82lfcRviezSUc18tlPM77GpCQ"
   },
   "expirationDate": "2022-12-16T01:46:04Z"
-}`
-DIDKit.verifyCredential(vc, "{}").then((value) => {
-  console.log("value", value)
+}
+const proofOption = {
+  "type": "Ed25519Signature2018",
+  "proofPurpose": "assertionMethod",
+  "verificationMethod": "did:web:demo.spruceid.com#_t-v-Ep7AtkELhhvAzCCDzy1O5Bn_z1CVFv9yiRXdHY",
+}
+
+verifyCredential(JSON.stringify(vc), JSON.stringify(proofOption)).then((res) => {
+  console.log("res", res)
+}).catch((err) => {
+  console.log("err", err)
 })
